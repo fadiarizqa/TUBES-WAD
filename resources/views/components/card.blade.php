90020d31853711fcab3cd444b245526dbd4392a4
Mellafesa <89730797+Mellafesa@users.noreply.github.com>
2025-06-01 17:13:20 +0700

    routing-dropdown-detail-barang

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,19 +1,16 @@
-<div class="max-w-xs bg-white border border-gray-200 rounded-xl overflow-hidden shadow-sm">
+@props(['nama', 'deskripsi', 'foto', 'id', 'type'])
+<div class="w-[270px] bg-white border border-gray-200 rounded-xl overflow-hidden shadow-sm flex flex-col justify-between">
     <div class=" rounded-t-xl"></div>
-        <img src="{{ asset('kimi.png') }}" alt="Logo"/>
+        <img class="w-full h-[160px] object-cover rounded-t-lg" src="{{ asset('storage/' . $foto) }}" alt="Foto Barang">
     <div class="p-4">
-        <h2 class="text-lg font-bold text-black">Nama Barang</h2>
+        <h2 class="text-lg font-bold text-black">{{ $nama }}</h2>
         <!-- buat p ini nanti nerima sebuah deskripsi singkat aja dari si barang  -->
-        <p class="text-sm text-gray-500 mt-1">
-            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-        </p>
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <!-- routenya blm bener -->
-            <button class="w-full text-black border-2 border-[#0F1123] text-sm mt-4 p-2 rounded-full hover:bg-[#0F1123] hover:!text-white cursor-pointer font-medium transition-colors duration-200">
-                Lihat Detail Barang
-            </button>
-        </form>
+        <p class="text-sm text-gray-500 mt-1">{{ Str::limit($deskripsi, 50) }}</p>
+        <!-- routing ke detail barangnya -->        
+        <a href="{{ $type == 'founded' ? route('founded_items.show', $id) : route('lost_items.show', $id) }}"
+           class="w-full text-black border-2 border-[#0F1123] text-sm mt-4 p-2 rounded-full hover:bg-[#0F1123] hover:text-white cursor-pointer font-medium transition-colors duration-200 inline-block text-center">
+           Lihat Detail Barang
+        </a>
         
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ Route::middleware('auth')->group(function() {
     Route::get('/founded_items', [FoundedItemController::class, 'index'])->name('founded_items.index');
     Route::get('/founded_items/create', [FoundedItemController::class, 'create'])->name('founded_items.create');
     Route::post('/founded_items', [FoundedItemController::class, 'store'])->name('founded_items.store');
+    Route::get('/founded_items/{id}', [FoundedItemController::class, 'show'])->name('founded_items.show');
+    Route::get('/lost_items/{id}', [LostItemController::class, 'show'])->name('lost_items.show');
     Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
+
 });
